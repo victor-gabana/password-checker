ad5b3789378dfd8ea3c453511d49702f5744718c
Maciej Matyjas <mmatyjas@expedia.com>
2016-10-07 09:19:24 +0100

    working a little builder pattern

--- password-checker.playground/Contents.swift
+++ password-checker.playground/Contents.swift
@@ -6,8 +6,8 @@
 import Cocoa
 import XCTest
 
-enum Strength: String {
-    case Feeble
+enum Strength: Int {
+    case Feeble = 0
     case Weak
     case Average
     case Strong
@@ -15,9 +15,42 @@ enum Strength: String {
 }
 
 func checkPassword(proposed: String) -> Strength {
-    return Strength.Average
+    
+    return PasswordStrengthBuilder(proposed).isLongEnough(limit: 12).build()
 }
 
-XCTAssertEqual(Strength.Average, checkPassword(proposed: "1q2w3"))
+class PasswordStrengthBuilder{
+    
+    let strength: Strength
+    let password: String
+
+    init(_ _password:String, strength _strength: Strength = Strength.Feeble) {
+        password = _password
+        strength = _strength
+    }
+    
+    func isLongEnough(limit: Int) -> PasswordStrengthBuilder {
+        
+        if password.characters.count >= limit {
+            return PasswordStrengthBuilder(password, strength: Strength(rawValue: strength.rawValue + 1)!) //hate!!
+        } else {
+            return PasswordStrengthBuilder(password, strength: strength)
+        }
+    }
+    
+    func build() -> Strength {
+       return strength
+    }
+}
+
+// initial state of builder should be feeble
+let psb = PasswordStrengthBuilder("")
+XCTAssertEqual(Strength.Feeble, psb.build())
+
+// can set my builder up
+let robustPSB = PasswordStrengthBuilder("", strength: .Robust)
+XCTAssertEqual(Strength.Robust, robustPSB.build())
+
+XCTAssertGreaterThan(checkPassword(proposed: "1q2w3"), Strength.Feeble)
 //XCTAssertEqual(Strength.Feeble, checkPassword(proposed: "password"))
 //XCTAssertEqual(Strength.Robust, checkPassword(proposed: "Vf(rB!K&Kh^9sU{U"))
